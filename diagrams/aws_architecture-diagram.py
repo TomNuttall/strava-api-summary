e047d87e16bde5d894d2cb476de89152d9c10369
Tom <tsnuttall@gmail.com>
2024-03-02 17:45:29 +0000

    diagrams: Update diagram

--- diagrams/aws_architecture-diagram.py
+++ diagrams/aws_architecture-diagram.py
@@ -13,17 +13,19 @@ with Diagram("", filename="aws_architecture-diagram", outformat="png"):
 
     with Cluster("AWS"):
         event = Eventbridge("EventBridge")
-        lambda_function = Lambda("Lambda")
 
-        event >> Edge(label="Run schedule once a week") >> lambda_function
+        with Cluster(""):
+            lambda_function = Lambda("Lambda")
 
-        lambda_function >> Edge(
-            label="Store / Retrive access token") >> KMS("KMS") >> Edge() << ParameterStore("ParameterStore")
-        lambda_function >> Edge(label="Send stats summary") >> SES("SES")
-        lambda_function >> Edge(
-            label="Logging") >> Cloudwatch("CloudWatch")
+            event >> Edge(label="Run schedule once a week") >> lambda_function
 
-        lambda_function >> Edge(
-            label="Request access token / athlete data") >> strava_api
+            lambda_function >> Edge(
+                label="Store / Retrive access token") >> KMS("KMS") >> Edge() << ParameterStore("ParameterStore")
+            lambda_function >> Edge(label="Send stats summary") >> SES("SES")
+            lambda_function >> Edge(
+                label="Logging") >> Cloudwatch("CloudWatch")
+
+            lambda_function >> Edge(
+              label="Request access token / athlete data") >> strava_api
 
     github_action >> Edge(label="Deploys lambda code") >> lambda_function
